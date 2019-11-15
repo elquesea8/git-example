5d9c473d76db9846d268af35497307d324d32b97
elquesea8 <50340140+elquesea8@users.noreply.github.com>
2019-11-15 10:29:08 -0800

    Add files via upload

---
+++ myProject/src/test/java/myProject/ReadExcelFile.java
@@ -0,0 +1,37 @@
+package myProject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcelFile {
+	private static final String FILE_NAME = "C:/Academia/MyFirstExcel.xlsx";
+	public static void main(String[] args) {
+		try {
+			FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
+			Workbook workbook = new XSSFWorkbook(excelFile);
+			Sheet datatypeSheet = workbook.getSheetAt(0);
+			Iterator<Row> iterator = datatypeSheet.iterator();
+			
+			while (iterator.hasNext()) {
+				Row currentRow = iterator.next();
+				Iterator<Cell> cellIterator = currentRow.iterator();
+				
+				while (cellIterator.hasNext()) {
+					Cell currentCell = cellIterator.next();
+					System.out.print(currentCell.getStringCellValue());
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
